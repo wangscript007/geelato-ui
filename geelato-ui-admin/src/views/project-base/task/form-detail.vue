3d7e4e0babfa68435b5fecb4498c4811b2ffbc8b
geelato <geelato@126.com>
2018-05-02 08:22:06 +0800

    update task

--- geelato-ui-admin/src/views/project-base/task/form-detail.vue
+++ geelato-ui-admin/src/views/project-base/task/form-detail.vue
@@ -16,21 +16,25 @@
           <label>任务标题</label>
           <input type="text" name="title" v-model="form.task.title" placeholder="标题">
         </div>
+        <!--<div class="field">-->
+        <!--<label>所属项目</label>-->
+        <!--<div class="ui fluid search selection dropdown projectList">-->
+        <!--<input type="hidden" name="receipt">-->
+        <!--<i class="dropdown icon"></i>-->
+        <!--<div class="default text">所属项目</div>-->
+        <!--<div class="menu">-->
+        <!--<template v-for="item in list.project">-->
+        <!--<div class="item" :data-value="item.id" :data-text="item.name">-->
+        <!--&lt;!&ndash;<img class="ui mini avatar image" src="/static/assets/images/avatar/small/geelato.jpg">&ndash;&gt;-->
+        <!--{{item.name}}-->
+        <!--</div>-->
+        <!--</template>-->
+        <!--</div>-->
+        <!--</div>-->
+        <!--</div>-->
         <div class="field">
-          <label>所属项目</label>
-          <div class="ui fluid search selection dropdown projectList">
-            <input type="hidden" name="receipt">
-            <i class="dropdown icon"></i>
-            <div class="default text">所属项目</div>
-            <div class="menu">
-              <template v-for="item in list.project">
-                <div class="item" :data-value="item.id" :data-text="item.name">
-                  <!--<img class="ui mini avatar image" src="/static/assets/images/avatar/small/geelato.jpg">-->
-                  {{item.name}}
-                </div>
-              </template>
-            </div>
-          </div>
+          <label>截止时间</label>
+          <input type="text">
         </div>
       </div>
       <div class="two fields">
@@ -211,9 +215,62 @@
           <td style="min-width: 5em;background-color: #eeeeee">指派给</td>
           <td><i class="user circle big icon"></i>张三</td>
         </tr>
+        <tr>
+          <td style="min-width: 5em;background-color: #eeeeee">标签&nbsp;<i class="tag icon"></i></td>
+          <td>
+            <div class="ui mini basic blue label">
+              报告
+              <i class="delete icon"></i>
+            </div>
+            <div class="ui mini basic yellow label">
+              方案
+              <i class="delete icon"></i>
+            </div>
+          </td>
+        </tr>
         </tbody>
       </table>
-      <h4 class="ui dividing header">相关任务</h4>
+      <h4 class="ui dividing header">任务成果(1/2)</h4>
+      <gl-group item=".list>.item">
+        <div class="ui aligned divided list">
+          <div class="item">
+            <div class="right floated content">
+              <i class="download big icon gl-state-finished"></i>
+            </div>
+            <div class="content" style="cursor: pointer" @click="$_toAddingAchievement()">
+              <i class="check circle outline big icon gl-state-none"></i>
+              《用户需求》
+            </div>
+          </div>
+          <div class="item">
+            <div class="right floated content">
+              <i class="download big icon gl-state-unfinished"></i>
+            </div>
+            <div class="content" style="cursor: pointer" @click="$_toAddingAchievement()">
+              <i class="circle outline big icon gl-state-none"></i>
+              《产品需求》
+            </div>
+          </div>
+          <div class="item" v-if="achievementState==='querying'">
+            <!--<div class="right floated content">-->
+            <!--<i class="user circle big icon"></i>-->
+            <!--</div>-->
+            <div class="content" style="cursor: pointer" @click="$_toAddingAchievement()">
+              <i class="add circle big icon"></i>
+              添加其它成果
+            </div>
+          </div>
+          <div class="item" v-if="achievementState==='editing'">
+            <div class="right floated content">
+              <i class="user circle big icon"></i>
+            </div>
+            <div class="content">
+              <input type="text" style="width: 80%" @keyup.enter="$_addAchievement($event)">
+            </div>
+          </div>
+        </div>
+      </gl-group>
+      <h4 class="ui dividing header">子任务(0/0)</h4>
       <gl-group item=".list>.item">
         <div class="ui aligned divided list">
           <div class="item" v-if="subTaskState==='editing'">
@@ -231,6 +288,28 @@
             <div class="content" style="cursor: pointer" @click="$_toAddingSubTask()">
               <i class="add circle big icon"></i>
               添加子任务
+            </div>
+          </div>
+        </div>
+      </gl-group>
+      <h4 class="ui dividing header">前置任务(0/0)</h4>
+      <gl-group item=".list>.item">
+        <div class="ui aligned divided list">
+          <div class="item" v-if="preTaskState==='editing'">
+            <div class="right floated content">
+              <i class="user circle big icon"></i>
+            </div>
+            <div class="content">
+              <input type="text" style="width: 80%" @keyup.enter="$_addPreTask($event)">
+            </div>
+          </div>
+          <div class="item" v-if="preTaskState==='querying'">
+            <div class="right floated content">
+              <i class="user circle big icon"></i>
+            </div>
+            <div class="content" style="cursor: pointer" @click="$_toAddingPreTask()">
+              <i class="add circle big icon"></i>
+              添加前置任务
             </div>
           </div>
         </div>
@@ -254,6 +333,10 @@
       return {
         // 子任务状态 editing querying
         subTaskState: 'querying',
+        // 前置任务状态 editing querying
+        preTaskState: 'querying',
+        // 任务成果 editing querying
+        achievementState: 'querying',
         // 动态信息，只展示评论
         showDynamicInfo: false,
         // 动态信息，只展示评论
@@ -332,12 +415,24 @@
       $_cancel () {
         this.$parent.pageState = 'querying'
       },
+      $_toAddingAchievement () {
+        this.achievementState = 'editing'
+      },
+      $_addAchievement (event) {
+        this.achievementState = 'querying'
+      },
       $_toAddingSubTask () {
         this.subTaskState = 'editing'
       },
       $_addSubTask (event) {
         console.log(event)
         this.subTaskState = 'querying'
+      },
+      $_toAddingPreTask () {
+        this.preTaskState = 'editing'
+      },
+      $_addPreTask (event) {
+        this.preTaskState = 'querying'
       }
     },
     components: {TaskComment}
